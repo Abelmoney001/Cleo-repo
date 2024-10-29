4aaa4bf34bff6daf479e140b73c2241579bcfde1
Osaretin Frank <osaretin.frank10@gmail.com>
2024-10-29 15:52:45 +0100

    updates

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,11 +4,16 @@ import React from "react"
 export const Video = () => {
   return (
     <div className="hero-img">
-      <img
-        src="/images/cleo.jpg"
+      <video
+        controls
+        poster="/images/cleo.jpg"
         className="md:h-[450px] h-[300px] w-full object-cover rounded-3xl"
-        alt=""
-      />
+        autoPlay
+        loop
+      >
+        <source src="/videos/cleo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
